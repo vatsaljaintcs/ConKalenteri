86bd2134c397d1a22a907e93f643dc6735dc9a0b
vatsaljaintcs <121945973+vatsaljaintcs@users.noreply.github.com>
2023-01-06 18:23:01 +0530

    Add files via upload

---
+++ src/screens/ActivitiesScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View ,useWindowDimensions} from 'react-native'
+import React from 'react'
+import LinearGradient from 'react-native-linear-gradient';
+import ForgotDetailsBox from '../components/organisms/ForgotPasswordDetailsBox';
+import NavHeader from '../components/molecules/navheader';
+import LocationBox from '../components/organisms/LocationBox';
+import ActivityListBox from '../components/organisms/ActivityListBox';
+
+const ActivitiesScreen = () => {
+    const hgth = useWindowDimensions().height;
+    const wid = useWindowDimensions().width;
+  return (
+    <View style={styles.container}>
+    <LinearGradient
+    colors={['rgb(245,165,229)', 'rgb(161,234,241)']}
+    start={{ x: 0, y: 0 }} 
+    end={{ x: 0.5, y: 0.4 }} 
+ 
+     style={{
+      
+       height: hgth*1,width:wid*1,justifyContent:'center'}}
+   >
+    
+    <NavHeader data={{flex:0.07,header:'Activities',back:true}} />
+   
+  <ActivityListBox data={{flex:0.93}} />
+  
+   </LinearGradient>
+ 
+ </View>
+  )
+}
+
+export default ActivitiesScreen
+
+const styles = StyleSheet.create({})
